3b6b3bbd4adbd99287c6267670b5214993d2b79f
TALIS_DEV <wordbit.bool@gmail.com>
2024-10-20 14:26:43 -0300

    first commit app started

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/assets/components/button/button';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Button text="Acessar" />
+
+
+      <Text>Open up App.js to start !</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/assets/components/button/button-style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+
+
+export const styles = StyleSheet.create ({
+
+btn : {  
+    backgroundColor: '#0D6EFD',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:15,
+    borderRadius:8,
+    margin:10,
+    paddingHorizontal:'30%',
+
+},
+
+text : {
+    color:'#fff',
+    fontSize:20,   
+}
+
+});
